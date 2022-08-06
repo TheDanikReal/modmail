3c4b9282031a226dd5345650199b420fb449f5a8
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2022-08-06 16:57:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,6 +21,17 @@ from discord.ext import commands, tasks
 from discord.ext.commands.view import StringView
 from emoji import UNICODE_EMOJI
 from pkg_resources import parse_version
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run()
+# Defining the home page of our site
+@app.route("/")  # this sets the route to this page
+def home():
+	return "Hello! this is the main page <h1>HELLO</h1>"  # some basic inline html
+
 
 from core.utils import tryint
 
